f43bd61e4a3bc90441498b9e7935f719f3c62e1f
Ricardo Hatsugai <rhatsugai@gmail.com>
2023-10-26 06:20:10 -0300

    Arquivo de exemplo do módulo bibliotecas

---
+++ bibliotecas/exemplo1.py
@@ -0,0 +1,13 @@
+from math import factorial
+from math import sqrt
+import datetime
+
+print(factorial(5))
+
+print(sqrt(25))
+
+data = datetime.date(2023, 9, 11)
+print(data)
+
+dataagora = datetime.datetime.now()
+print(dataagora)
